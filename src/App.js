81645982ad9d5a6fd851592472dd9ded690cafd9
liliput35 <83822871+liliput35@users.noreply.github.com>
2021-10-20 09:09:49 +0800

    deploy my app to pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+ constructor(props){
+   super(props)
+
+   this.state={
+     newItem: "", 
+     list: [], 
+   }
+ }
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem(){
+    const newItem = {
+      id: 1 + Math.random(), 
+      value: this.state.newItem.slice()
+    }
+
+    const list = [...this.state.list]
+    list.push(newItem)
+
+    this.setState({
+      newItem: "", 
+      list, 
+    })
+  }
+
+  deleteItem(id){
+    const list = [...this.state.list]
+    const updatedList = list.filter(item => item.id !== id)
+
+    this.setState({
+      list: updatedList
+    })
+  }
+   render(){
+     let update = this.state.list.length ? '' : 'Nothing to do yet. Add an item'
+     return(
+       <div className="container">
+         <header>
+           <h2>Ciocon's Todolist</h2>
+
+           <div className="form">
+             <input 
+             type="text" 
+             placeholder="Add Item"
+             value={this.state.newItem}
+             onChange={e => this.updateInput("newItem", e.target.value)}
+             />
+             <button onClick={() => this.addItem()}>
+               +
+             </button>
+           </div>
+         </header>
+         <section>
+           <ul>
+             {this.state.list.map(item => {
+               return(
+                 <li key={item.id}>{item.value}
+                 <button onClick={() => this.deleteItem(item.id)}>X</button>
+                 </li>
+               )
+             })}
+           </ul>
+           <p>{update}</p>
+         </section>
+       </div>
+     )
+   }
+ 
 }
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
